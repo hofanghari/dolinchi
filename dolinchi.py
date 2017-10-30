ee07d9674e1007346a01f8f4db647fa575736bf0
HoangHai <hofanghari@gmail.com>
2017-10-31 01:11:33 +0700

    dolinchi

---
+++ dolinchi.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+from selenium import webdriver
+import org.openqa.selenium.support.ui.Select as Select
+import time
+
+
+class Chilindo(object):
+    """docstring for Chilindo"""
+
+    def __init__(self, usr, pwd):
+        self.err = ''
+        self.usr = usr
+        self.pwd = pwd
+        self.login()
+
+    def beat(self, url, price, sel=0):
+        self.driver.get("http://www.chilindo.com/vn/product/%s" % url)
+        self.driver.find_element_by_id('ContentPlaceHolder1_txtBidNew').clear()
+        self.driver.find_element_by_id(
+            'ContentPlaceHolder1_txtBidNew').send_keys(price)
+        select = Select(self.driver.find_element_by_id(
+            'ContentPlaceHolder1_ddlRelatedItems'))
+        select.selectByVisibleText(sel)
+        select.selectByIndex(sel)
+        time.sleep(2)
+        self.driver.find_element_by_id('ContentPlaceHolder1_btnBid').click()
+
+    def login(self):
+        self.driver = webdriver.Firefox()
+        # caps = webdriver.DesiredCapabilities.FIREFOX.copy()
+        # driver = webdriver.Remote(desired_capabilities=caps)
+        self.driver.get('http://www.chilindo.com')
+        try:
+            self.driver.find_element_by_id('lnkSelectCountry').click()
+            time.sleep(1)
+            self.driver.find_element_by_id('popsignin').click()
+            time.sleep(1)
+            self.driver.find_element_by_id('mutedlogin').click()
+            time.sleep(1)
+            self.driver.find_element_by_id(
+                'Signin1_txtEmail').send_keys(self.usr)
+            self.driver.find_element_by_id(
+                'Signin1_txtPassword').send_keys(self.pwd)
+            time.sleep(1)
+            self.driver.find_element_by_id('Signin1_btnSignIn').click()
+        except:
+            self.err = 'can not login'
+
+
+def main():
+    # profile = webdriver.FirefoxProfile(profile_path)
+    # profile = webdriver.FirefoxProfile("/home/hoanghai/.mozilla/firefox/sakntkwa.chilindo/")
+    # print(profile)
+    # driver = webdriver.Firefox(firefox_profile=profile)
+    # driver = webdriver.Firefox(firefox_profile='/home/hoanghai/.mozilla/firefox/sakntkwa.chilindo')
+    # fp = webdriver.FirefoxProfile.path()
+    # print(profile)
+    # set something on the profile...
+    # driver = webdriver.Firefox(firefox_profile=fp)
+    chi = Chilindo('hoanghaikgs', '256487')
+    time.sleep(1)
+    chi.beat('42-036', 200, 1)
+    # url = 'http://www.chilindo.com/vn/product/41-030'
+    # price = 130
+    # beat(driver, url, price)
+    # driver = webdriver.Firefox()
+    # driver.get("http://www.python.org")
+
+
+if __name__ == '__main__':
+    main()
